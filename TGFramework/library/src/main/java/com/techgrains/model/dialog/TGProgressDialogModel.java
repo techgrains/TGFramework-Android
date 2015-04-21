742c3d2eb7689f5c51dd9730c1bbf2567275991b
Vinayraj Singh <vinayrraj@gmail.com>
2015-04-21 17:50:16 +0530

    Added IndeterminateDrawable for progress dialog

--- TGFramework/library/src/main/java/com/techgrains/model/dialog/TGProgressDialogModel.java
+++ TGFramework/library/src/main/java/com/techgrains/model/dialog/TGProgressDialogModel.java
@@ -1,8 +1,6 @@
 package com.techgrains.model.dialog;
 
-/**
- * Created by CNS on 21/04/15.
- */
+import android.support.annotation.DrawableRes;
 
 /**
  * Created by CNS on 21/04/15.
@@ -12,6 +10,29 @@ public class TGProgressDialogModel {
     private String title;
     private String message = null;
     private boolean cancelable;
+    private int indeterminateDrawable = -1;
+
+    public TGProgressDialogModel(String title, String message, boolean cancelable) {
+        this.title = title;
+        this.message = message;
+        this.cancelable = cancelable;
+    }
+
+    public TGProgressDialogModel(String title, String message, boolean cancelable, @DrawableRes int indeterminateDrawable) {
+
+        this.title = title;
+        this.message = message;
+        this.cancelable = cancelable;
+        this.indeterminateDrawable = indeterminateDrawable;
+    }
+
+    public int getIndeterminateDrawable() {
+        return indeterminateDrawable;
+    }
+
+    public void setIndeterminateDrawable(@DrawableRes int indeterminateDrawable) {
+        this.indeterminateDrawable = indeterminateDrawable;
+    }
 
     public boolean isCancelable() {
         return cancelable;
