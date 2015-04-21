774f638630fceddeb21b180c49c4d00ea3e790cd
Vinayraj Singh <vinayrraj@gmail.com>
2015-04-21 18:04:30 +0530

    TGDialog Model Test

---
+++ TGFramework/library/src/androidTest/java/com/techgrains/model/dialog/TGDialogModelTest.java
@@ -0,0 +1,57 @@
+package com.techgrains.model.dialog;
+
+import android.content.DialogInterface;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+
+/**
+ * Created by CNS on 21/04/15.
+ */
+public class TGDialogModelTest extends TestCase {
+
+    TGDialogModel model;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testCreateObject() {
+        String title = "Title";
+        String message = "message";
+        String positiveText = "positiveText";
+        DialogInterface.OnClickListener onPositiveCLick = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        };
+        String negativeText = "negativeText";
+        DialogInterface.OnClickListener onNegativeClick = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        };
+        boolean cancellable = false;
+
+        model = new TGDialogModel(title, message, positiveText, onPositiveCLick, negativeText, onNegativeClick, cancellable);
+
+        Assert.assertTrue(model.getTitle().equals(title));
+        Assert.assertTrue(model.getMessage().equals(message));
+        Assert.assertTrue(model.getPositiveText().equals(positiveText));
+        Assert.assertEquals(onPositiveCLick, model.getOnPositiveCLick());
+
+        Assert.assertTrue(model.getNegativeText().equals(negativeText));
+        Assert.assertEquals(onNegativeClick, model.getOnNegativeClick());
+
+        Assert.assertTrue(model.isCancellable() == cancellable);
+    }
+}
