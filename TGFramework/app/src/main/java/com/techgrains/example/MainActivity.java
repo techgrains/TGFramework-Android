74afc4f9763b6d1ac9e18bd024396d0d8aad61a7
Vinayraj Singh <vinayrraj@gmail.com>
2015-04-28 12:21:08 +0530

    Removed AppCompact lib (which was providing Support 4v lib).

--- TGFramework/app/src/main/java/com/techgrains/example/MainActivity.java
+++ TGFramework/app/src/main/java/com/techgrains/example/MainActivity.java
@@ -15,13 +15,13 @@
  */
 package com.techgrains.example;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
